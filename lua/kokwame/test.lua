279000b98aaf33b9e94715efa34e7b4a296bdff7
Per Juchtmans <per@dubgeiser.net>
2023-07-20 10:37:03 +0200

    Refactor test.lua
    Extract printing query matches in separate function.

--- lua/kokwame/test.lua
+++ lua/kokwame/test.lua
@@ -1,13 +1,21 @@
 local M = {}
 
+local function print_query_matches(node, buf, query_name)
+  local query = vim.treesitter.query.get(vim.bo.ft, query_name)
+  for _, captures, metadata in query:iter_matches(node, buf) do
+    if #captures >= 1 then
+      print(vim.inspect(vim.treesitter.get_node_text(captures[1], buf)))
+    else
+      print('empty, else clause?')
+    end
+  end
+end
+
 function M.test()
   local buf = vim.api.nvim_get_current_buf()
   local root = vim.treesitter.get_parser(buf, vim.bo.ft):parse()[1]:root()
-  local query = vim.treesitter.query.get(vim.bo.ft, 'kokwame-conditionals')
-
-  for _, captures, metadata in query:iter_matches(root, buf) do
-    print(vim.inspect(vim.treesitter.get_node_text(captures[1], buf)))
-  end
+  print_query_matches(root, buf, 'kokwame-functions')
+  print_query_matches(root, buf, 'kokwame-conditionals')
 end
 
 return M
